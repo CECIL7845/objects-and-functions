c939ca449b813b700170b5bb17a439d0efad2c0c
CECIL7845 <odonkorcecil123@gmail.com>
2023-09-27 20:23:28 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Cecil",
+    age: 23,
+    country: "Ghana"
+}
+
+function logData() {
+ console.log(person.name + " is " + person.age + " years old and lives in " + person.country )
+}
+
+logData()
